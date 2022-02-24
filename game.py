d45f3dc782e060085591798b4b7b54377e1de545
22cath <cecollignon@gmail.com>
2022-02-24 19:27:20 +0100

    fin de partida y 3 vidas

--- game.py
+++ game.py
@@ -3,6 +3,9 @@ import pygame as pg
 import random 
 pg.init()
 
+FPS = 60
+
+
 class Vigneta:
     def __init__(self, padre, x, y, ancho, alto, color = (255, 255, 255)):
         self.padre = padre
@@ -14,20 +17,45 @@ class Vigneta:
         self.vx = 0
         self.vy = 0
 
+    @property
+    def xcentro(self):
+        return self.x + self.ancho // 2
+
+    @property
+    def ycentro(self):
+        return self.y + self.alto // 2
+
+    def intersecta(self, otro) -> bool:
+        return (self.x in range(otro.x, otro.x + otro.ancho) or \
+                self.x + self.ancho in range(otro.x, otro.x + otro.ancho)) and \
+               (self.y in range(otro.y, otro.y + otro.alto) or \
+                self.y + self.alto in range(otro.y, otro.y + otro.alto))
+
+
     def dibujar(self):
         pass
 
     def mover(self):
         pass
 
+
+
 class Ladrillo(Vigneta):
+    """
+    def __init__(self, padre, x, y, ancho, alto, color = (255, 255, 255)):
+        super().__init__(padre, x, y, ancho, alto, color)
+        self.vivo = True
+    """
+
     def dibujar(self):
         pg.draw.rect(self.padre, self.color, (self.x, self.y, self.ancho, self.alto))
 
     def comprobarToque(self, bola):
-        #hago cosas al tocarme la bola
-        pass
+        if self.intersecta(bola):
+            bola.vy *= -1
+            return True
 
+        return False
 
 class Raqueta(Vigneta):
     def __init__(self, padre, x, y, ancho, alto, color = (255, 255, 0)):
@@ -49,38 +77,42 @@ class Raqueta(Vigneta):
         if self.x + self.ancho >= self.padre.get_width():
             self.x = self.padre.get_width() - self.ancho
 
-class Bola:
-    def __init__(self, padre, x, y, color = (255, 255, 255), radio = 10) :
-        self.x = x
-        self.y = y
-        self.color = color
+
+class Bola(Vigneta):
+    def __init__(self, padre, x, y, color = (255, 255, 255), radio = 10):
+        super().__init__(padre, x - radio, y - radio, 2 * radio, 2 * radio, color)
         self.radio = radio
         self.vx = 5
         self.vy = 5
-        self.padre = padre
+        self.x_ini = x
+        self.y_ini = y
+        self.esta_viva = True
 
+    def reset(self):
+        self.x = self.x_ini
+        self.y = self.y_ini
+        self.vx = 5
+        self.vy = 5
+        self.esta_viva = True
 
     def mover(self):
         self.x += self.vx 
         self.y += self.vy
 
-        if self.x <= self.radio or self.x >= self.padre.get_width() - self.radio:
+        if self.x <= 0 or self.x >= self.padre.get_width() - self.ancho:
             self.vx *= -1
 
-        #if self.radio >= self.x >= limDer - self.radio:
-
-        if self.y <= self.radio or self.y >= self.padre.get_height() - self.radio:
+        if self.y <= 0:
             self.vy *= -1
 
+        if self.y >= self.padre.get_height() - self.alto:
+            self.esta_viva = False
+
     def dibujar(self):
-        pg.draw.circle(self.padre, self.color, (self.x, self.y), self.radio)
+        pg.draw.circle(self.padre, self.color, (self.xcentro, self.ycentro), self.radio)
 
     def compruebaChoque(self, otro):
-        if (self.x - self.radio in range(otro.x, otro.x + otro.ancho) or \
-            self.x + self.radio in range(otro.x, otro.x + otro.ancho)) and \
-           (self.y - self.radio in range(otro.y, otro.y + otro.alto) or \
-            self.y + self.radio in range(otro.y, otro.y + otro.alto)):
-           
+        if self.intersecta(otro):
            self.vy *= -1
 
 
@@ -88,9 +120,14 @@ class Bola:
 class Game:
     def __init__(self, ancho=600, alto=800):
         self.pantalla = pg.display.set_mode((ancho, alto))
-        self.bola = Bola(self.pantalla, ancho // 2, alto //2 )
-        self.raqueta = Raqueta(self.pantalla, ancho//2, alto // 2, 100, 20)
-        self.ladrillo = Ladrillo(self.pantalla, 10, 10, 100, 50)
+        self.bola = Bola(self.pantalla, ancho // 2, alto // 2)
+        self.raqueta = Raqueta(self.pantalla, ancho//2, alto - 30, 100, 20)
+        self.ladrillos = []
+        self.contador_vidas = 3
+
+        self.crea_ladrillos()
+
+        self.reloj = pg.time.Clock()
         """
         for i in range(random.randint(2, 10)):
             radio = random.randint(5, 50)
@@ -105,11 +142,17 @@ class Game:
             self.bolas[i].vy = random.randint(5,15) * random.choice[(-1, 1)]
         """
 
+    def crea_ladrillos(self):
+        for col in range(10):
+            for fil in range(4):
+                l = Ladrillo(self.pantalla, 5 + 60 * col, 25 + 30 * fil, 50, 20)
+                self.ladrillos.append(l)
 
     def bucle_ppal(self):
         game_over = False
 
-        while not game_over:
+        while self.contador_vidas > 0 and not game_over:
+            self.reloj.tick(FPS)
 
             eventos = pg.event.get()
             for evento in eventos:
@@ -129,14 +172,32 @@ class Game:
 
             self.pantalla.fill((255, 0, 0))    
 
-            self.ladrillo.mover()
             self.bola.mover()
             self.raqueta.mover()
             self.bola.compruebaChoque(self.raqueta)
+
+            if not self.bola.esta_viva:
+                self.contador_vidas -= 1
+                self.bola.reset()
+    
             self.bola.dibujar()
             self.raqueta.dibujar()
-            self.ladrillo.dibujar()
+            """
+            for ladrillo in self.ladrillos:
+                if ladrillo.vivo:
+                    ladrillo.comprobarToque(self.bola)
+                    ladrillo.dibujar()
+            for indice, ladrillo in enumerate(self.ladrillos):
+                ha_chocado = ladrillo.comprobarToque(self.bola)
+                if ha_chocado:
+                    self.ladrillos.pop(indice)
+            """
+            for ladrillo in self.ladrillos:
+                if ladrillo.comprobarToque(self.bola):
+                    self.ladrillos.remove(ladrillo)
+                ladrillo.dibujar()
 
+            print(len(self.ladrillos))
             pg.display.flip()
 
     
