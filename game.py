79bea540fae65f3ce34f3eaf8d9f89e5544651cb
22cath <cecollignon@gmail.com>
2022-02-24 21:27:11 +0100

    niveles con while dentro de while

--- game.py
+++ game.py
@@ -6,6 +6,14 @@ pg.init()
 FPS = 60
 
 
+niveles = [
+           [(0,0), (1,1), (2,2), (3, 3)],
+           [(0, 0), (1, 0),(2, 0),(3, 0),(4, 0),(5, 0),(6, 0),(7, 0),(8, 0),(9, 0), 
+            (0, 1), (9, 1),
+            (0, 2), (9, 2),
+            (0, 3), (1, 3),(2, 3),(3, 3),(4, 3),(5, 3),(6, 3),(7, 3),(8, 3),(9, 3)],
+           ] 
+
 class Vigneta:
     def __init__(self, padre, x, y, ancho, alto, color = (255, 255, 255)):
         self.padre = padre
@@ -26,10 +34,30 @@ class Vigneta:
         return self.y + self.alto // 2
 
     def intersecta(self, otro) -> bool:
+
+        if self.ancho > otro.ancho:
+            menor_ancho = otro
+            mayor_ancho = self
+        else:
+            menor_ancho = self
+            mayor_ancho = otro
+
+        if self.alto > otro.alto:
+            menor_alto = otro
+            mayor_alto = self
+        else:
+            menor_alto = self
+            mayor_alto = otro
+        return (menor_ancho.x in range(mayor_ancho.x, mayor_ancho.x + mayor_ancho.ancho) or \
+                menor_ancho.x + menor_ancho.ancho in range(mayor_ancho.x, mayor_ancho.x + mayor_ancho.ancho)) and \
+               (menor_alto.y in range(mayor_alto.y, mayor_alto.y + mayor_alto.alto) or \
+                menor_alto.y + menor_alto.alto in range(mayor_alto.y, mayor_alto.y + mayor_alto.alto))
+        """
         return (self.x in range(otro.x, otro.x + otro.ancho) or \
                 self.x + self.ancho in range(otro.x, otro.x + otro.ancho)) and \
                (self.y in range(otro.y, otro.y + otro.alto) or \
                 self.y + self.alto in range(otro.y, otro.y + otro.alto))
+        """                 
 
 
     def dibujar(self):
@@ -37,8 +65,6 @@ class Vigneta:
 
     def mover(self):
         pass
-
-
 
 class Ladrillo(Vigneta):
     """
@@ -77,7 +103,6 @@ class Raqueta(Vigneta):
         if self.x + self.ancho >= self.padre.get_width():
             self.x = self.padre.get_width() - self.ancho
 
-
 class Bola(Vigneta):
     def __init__(self, padre, x, y, color = (255, 255, 255), radio = 10):
         super().__init__(padre, x - radio, y - radio, 2 * radio, 2 * radio, color)
@@ -115,8 +140,6 @@ class Bola(Vigneta):
         if self.intersecta(otro):
            self.vy *= -1
 
-
-
 class Game:
     def __init__(self, ancho=600, alto=800):
         self.pantalla = pg.display.set_mode((ancho, alto))
@@ -124,8 +147,6 @@ class Game:
         self.raqueta = Raqueta(self.pantalla, ancho//2, alto - 30, 100, 20)
         self.ladrillos = []
         self.contador_vidas = 3
-
-        self.crea_ladrillos()
 
         self.reloj = pg.time.Clock()
         """
@@ -142,64 +163,72 @@ class Game:
             self.bolas[i].vy = random.randint(5,15) * random.choice[(-1, 1)]
         """
 
-    def crea_ladrillos(self):
-        for col in range(10):
-            for fil in range(4):
-                l = Ladrillo(self.pantalla, 5 + 60 * col, 25 + 30 * fil, 50, 20)
-                self.ladrillos.append(l)
+    def crea_ladrillos(self, nivel):
+        for col, fil in niveles[nivel]:
+            l = Ladrillo(self.pantalla, 5 + 60 * col, 25 + 30 * fil, 50, 20)
+            self.ladrillos.append(l)
 
     def bucle_ppal(self):
         game_over = False
+        nivel = 0
 
-        while self.contador_vidas > 0 and not game_over:
-            self.reloj.tick(FPS)
+        while self.contador_vidas > 0 and nivel < len(niveles):
+            self.crea_ladrillos(nivel)
 
-            eventos = pg.event.get()
-            for evento in eventos:
-                if evento.type == pg.QUIT:
-                    game_over = True
+            while self.contador_vidas > 0 and len(self.ladrillos) > 0: 
+                # Este if equivale a
+                # and  len(self.ladrillos) > 0
+                # puesto en la línea del while
+                
+                self.reloj.tick(FPS)
+
+                eventos = pg.event.get()
+                for evento in eventos:
+                    if evento.type == pg.QUIT:
+                        self.contador_vidas = 0
+                    """
+                    if evento.type == pg.KEYDOWN:
+                        if evento.key == pg.K_LEFT:
+                            self.raqueta.vx = -5
+                        if evento.key == pg.K_RIGHT:
+                            self.raqueta.vx = 5
+                    if evento.type == pg.KEYUP:
+                        if evento.key in (pg.K_LEFT, pg.K_RIGHT):
+                            self.raqueta.vx = 0
+                    """
+                
+
+                self.pantalla.fill((255, 0, 0))    
+
+                self.bola.mover()
+                self.raqueta.mover()
+                self.bola.compruebaChoque(self.raqueta)
+
+                if not self.bola.esta_viva:
+                    self.contador_vidas -= 1
+                    self.bola.reset()
+        
+                self.bola.dibujar()
+                self.raqueta.dibujar()
                 """
-                if evento.type == pg.KEYDOWN:
-                    if evento.key == pg.K_LEFT:
-                        self.raqueta.vx = -5
-                    if evento.key == pg.K_RIGHT:
-                        self.raqueta.vx = 5
-                if evento.type == pg.KEYUP:
-                    if evento.key in (pg.K_LEFT, pg.K_RIGHT):
-                        self.raqueta.vx = 0
+                for ladrillo in self.ladrillos:
+                    if ladrillo.vivo:
+                        ladrillo.comprobarToque(self.bola)
+                        ladrillo.dibujar()
+                for indice, ladrillo in enumerate(self.ladrillos):
+                    ha_chocado = ladrillo.comprobarToque(self.bola)
+                    if ha_chocado:
+                        self.ladrillos.pop(indice)
                 """
-            
-
-            self.pantalla.fill((255, 0, 0))    
-
-            self.bola.mover()
-            self.raqueta.mover()
-            self.bola.compruebaChoque(self.raqueta)
-
-            if not self.bola.esta_viva:
-                self.contador_vidas -= 1
-                self.bola.reset()
-    
-            self.bola.dibujar()
-            self.raqueta.dibujar()
-            """
-            for ladrillo in self.ladrillos:
-                if ladrillo.vivo:
-                    ladrillo.comprobarToque(self.bola)
+                for ladrillo in self.ladrillos:
+                    if ladrillo.comprobarToque(self.bola):
+                        self.ladrillos.remove(ladrillo)
                     ladrillo.dibujar()
-            for indice, ladrillo in enumerate(self.ladrillos):
-                ha_chocado = ladrillo.comprobarToque(self.bola)
-                if ha_chocado:
-                    self.ladrillos.pop(indice)
-            """
-            for ladrillo in self.ladrillos:
-                if ladrillo.comprobarToque(self.bola):
-                    self.ladrillos.remove(ladrillo)
-                ladrillo.dibujar()
 
-            print(len(self.ladrillos))
-            pg.display.flip()
+                print(len(self.ladrillos))
+                pg.display.flip()
 
+            nivel += 1
     
 if __name__ == '__main__':
     pg.init()
